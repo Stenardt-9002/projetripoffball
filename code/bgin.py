f867a9230a9a69c230448933f7af3912d3431e30
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2019-08-24 01:43:46 +0530

    part2

--- code/bgin.py
+++ code/bgin.py
@@ -3,6 +3,9 @@ import turtle
 wn = turtle.Screen()
 wn.bgcolor("BLack")
 wn.title("BOUNCING BALL")
+wn.tracer(0) #everything on screen dissapears
+
+
 
 ball = turtle.Turtle()
 ball.shape("circle")
@@ -12,12 +15,23 @@ ball.speed(0)#animation spped
 ball.goto(0,200)
 # ball.goto(0,200)
 ball.dy = 0
+ball.dx = 2 # setting x direction velocity
 
 gravity = 0.1
 while True:
+    wn.update()
     ball.dy -= gravity 
     ball.sety(ball.ycor()+ball.dy)
+
+    #check for wall collision
+    ball.setx(ball.xcor()+ball.dx)
+    if ball.xcor()>300 or ball.xcor()<-300:
+        ball.dx*=-1
+
     #chck bounce
+
+
+
     if ball.ycor()<-300:
         ball.dy*=-1
 
