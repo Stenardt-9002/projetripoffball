4fabb0100bc134ef645b3d26a74ca4139b236a45
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2019-09-03 01:51:33 +0530

    lec3 randomise y and x

--- code/bgin.py
+++ code/bgin.py
@@ -1,40 +1,51 @@
 import turtle
-
+import random
 wn = turtle.Screen()
 wn.bgcolor("BLack")
 wn.title("BOUNCING BALL")
 wn.tracer(0) #everything on screen dissapears
 
+colrs = ["white","red","blue","green"]
 
+mul_bals = []
+for _ in range(10):
+    mul_bals.append(turtle.Turtle())
 
-ball = turtle.Turtle()
-ball.shape("circle")
-ball.color("yellow")
-ball.penup()
-ball.speed(0)#animation spped
-ball.goto(0,200)
+for ball in mul_bals:
+    # ball = turtle.Turtle()
+    ball.shape("circle")
+    co_no = random.randint(0,3)
+    ball.color(colrs[co_no])
+    ball.penup() #doesn draw
+    ball.speed(0)#animation spped
+    x = random.randint(-290,290)
+    y = random.randint(-290,290)
+    ball.goto(x,y)
 # ball.goto(0,200)
-ball.dy = 0
-ball.dx = 2 # setting x direction velocity
+    ball.dy = 0
+    ball.dx = 2 # setting x direction velocity
 
 gravity = 0.1
 while True:
-    wn.update()
-    ball.dy -= gravity 
-    ball.sety(ball.ycor()+ball.dy)
 
-    #check for wall collision
-    ball.setx(ball.xcor()+ball.dx)
-    if ball.xcor()>300 or ball.xcor()<-300:
-        ball.dx*=-1
+    for ball in mul_bals:
+        wn.update()
+        ball.dy -= gravity
+        ball.sety(ball.ycor()+ball.dy)
 
-    #chck bounce
+        #check for wall collision
+        ball.setx(ball.xcor()+ball.dx)
+        # x direction movement
+        if ball.xcor()>300 or ball.xcor()<-300:
+            ball.dx*=-1
 
-
-
-    if ball.ycor()<-300:
-        ball.dy*=-1
+        #chck bounce
 
 
 
-wn.mainloop()
+        if ball.ycor()<-300:
+            ball.dy*=-1
+
+
+
+wn.mainloop() #kkeps window up
